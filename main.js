a7f14f2a10dc6d952d08efc56dfb836864b9f9df
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2024-09-07 15:38:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ const buttonSend = document.querySelector(".js-button");
 
 buttonSend.addEventListener("click", clicked);
 function clicked() {
+  const trueTime = new Date().toLocaleString("en-Us", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
   const newMessage = document.createElement("div");
-  newMessage.innerHTML = inputTarget.value;
-  console.log(inputTarget.value);
   newMessage.classList.add("user-message");
+  const message = document.createElement("p");
+  message.innerHTML = inputTarget.value;
+  const timeHtml = document.createElement("p");
+  timeHtml.innerHTML = trueTime;
+  timeHtml.classList.add("user-timer");
+  timeHtml.style.width = "65px";
+  timeHtml.style.bottom = "-16px";
+  newMessage.append(message, timeHtml);
   mainMessages.appendChild(newMessage);
   inputTarget.value = "";
   alertMessage.style.marginTop = "1.5rem";
